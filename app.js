edee812a5fb9ea979d4909f6e430f5336acb3dd3
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-07-20 16:53:47 -0700

    Make html skeleton, start JS objects

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+const Gameboard = (() => {
+    let gameboard = ['','','','','','','','','']
+    
+    let render = () => {
+        
+    }
+})();
+
+const PlayerFactory = (playerName, marker) => {
+    const sayName = () => console.log(`My name is ${playerName}`);
+
+    return { playerName, marker, sayName };
+}
