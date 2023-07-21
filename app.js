262c38b2572ffd746434856d60c18c2672f93479
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-07-21 16:57:13 -0700

    Starting logic over

--- app.js
+++ app.js
@@ -1,27 +1,51 @@
-const Gameboard = (() => {
-    const gameboard = ['X','O','O','O','O','X','X','X','X']
-    
-    const render = () => {
-        const boardDisplay = document.querySelector("#gameboard");
-        boardDisplay.innerHTML = '';
+//Setting up gameboard module
+const gameboardModule = (() => {
+    const gameboard = ['','','','','','','','','',]
 
-        gameboard.forEach((el, index) => {
-            const square = document.createElement("div");
-            square.classList.add("square");
-            square.setAttribute("id", `${index}`);
-            square.innerHTML = `<h1>${el}</h1>`;
-            boardDisplay.appendChild(square);
-            
-        })        
-    }
-
-    const startButton = document.querySelector("#start-game");
-    startButton.addEventListener("click", render);
+    return {};
 })();
 
+//setting up displaycontroller module
+const displayControllerModule = (() => {
+    const testDisplay = () => console.log("Test works");
+
+    return { testDisplay };
+})();
+
+//setting up player factory function
 const PlayerFactory = (playerName, marker) => {
     const sayName = () => console.log(`My name is ${playerName}`);
 
     return { playerName, marker, sayName };
 }
 
+const test = displayControllerModule;
+
+test.testDisplay();
+
+
+
+
+
+
+
+
+
+
+
+// const render = () => {
+    //     const boardDisplay = document.querySelector("#gameboard");
+    //     boardDisplay.innerHTML = '';
+
+    //     gameboard.forEach((el, index) => {
+    //         const square = document.createElement("div");
+    //         square.classList.add("square");
+    //         square.setAttribute("id", `${index}`);
+    //         square.innerHTML = `<h1>${el}</h1>`;
+    //         boardDisplay.appendChild(square);
+            
+    //     })        
+    // }
+
+    // const startButton = document.querySelector("#start-game");
+    // startButton.addEventListener("click", render);
