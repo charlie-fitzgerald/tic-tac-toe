17430e2a6d1bf277f408f83c6dc7bbab715af2ba
Charlie Fitzgerald <46912450+charlie-fitzgerald@users.noreply.github.com>
2023-07-21 18:39:13 -0700

    l;dakdfjasl;dkjg

--- app.js
+++ app.js
@@ -1,51 +1,50 @@
 //Setting up gameboard module
-const gameboardModule = (() => {
+const gameBoardModule = (() => {
     const gameboard = ['','','','','','','','','',]
 
-    return {};
+    //clicking start game button renders the board on screen
+    //will also be used to refresh the game board with the 
+    //latest game state after a player makes a move
+
+    const render = () => {
+        const boardDisplay = document.querySelector("#gameboard");
+        boardDisplay.innerHTML = '';
+
+        gameboard.forEach((el, index) => {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            square.setAttribute("id", `${index}`);
+            square.innerHTML = `<h1>${el}</h1>`;
+            boardDisplay.appendChild(square);
+            
+        })        
+    }
+
+    const getBoard = () => gameboard;
+
+    const startButton = document.querySelector("#start-game");
+    startButton.addEventListener("click", render);
+
+    return { getBoard };
 })();
 
 //setting up displaycontroller module
 const displayControllerModule = (() => {
-    const testDisplay = () => console.log("Test works");
-
-    return { testDisplay };
+        
 })();
 
 //setting up player factory function
-const PlayerFactory = (playerName, marker) => {
+const PlayerFactory = (playerName, playerId, marker) => {
     const sayName = () => console.log(`My name is ${playerName}`);
 
-    return { playerName, marker, sayName };
+    return { playerName, playerId, marker, sayName };
 }
 
-const test = displayControllerModule;
-
-test.testDisplay();
-
+let terry = PlayerFactory("Terry", 1, "X");
+let larry = PlayerFactory("Larry", 2, "O");
 
 
 
 
 
 
-
-
-
-
-// const render = () => {
-    //     const boardDisplay = document.querySelector("#gameboard");
-    //     boardDisplay.innerHTML = '';
-
-    //     gameboard.forEach((el, index) => {
-    //         const square = document.createElement("div");
-    //         square.classList.add("square");
-    //         square.setAttribute("id", `${index}`);
-    //         square.innerHTML = `<h1>${el}</h1>`;
-    //         boardDisplay.appendChild(square);
-            
-    //     })        
-    // }
-
-    // const startButton = document.querySelector("#start-game");
-    // startButton.addEventListener("click", render);
